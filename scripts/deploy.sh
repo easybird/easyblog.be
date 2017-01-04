4837941436461dfc13e885e6e7177529c6f2bb6b
jonathan dierckens <jonathan.dierckens@persgroep.be>
2017-01-04 09:24:40 +0100

    add autodeploy from travis

---
+++ scripts/deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+GIT_DEPLOY_REPO=${GIT_DEPLOY_REPO:-$(node -e 'process.stdout.write(require("./package.json").repository)')}
+
+if [ "$TRAVIS" = "true" ]
+then
+  # git need this, on Travis-CI nobody is defined
+  git config --global user.name "Easybird from Travis" && \
+  git config --global user.email "easybird85@gmail.com"
+fi
+
+# Move other necessary files to dist folder
+cp ./.travis.yml dist
+
+cd dist && \
+$(npm bin)/rimraf .git
+git init && \
+git add . && \
+git commit -m "Deploy static site" && \
+git push --force "${GIT_DEPLOY_REPO}" master:master
